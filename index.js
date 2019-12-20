b62f7f479f95b35a4131ffb5c545cdf777c22b6f
yhchoi <yhchoi@rsupport.com>
2019-12-20 16:02:34 +0900

    build: 0.0.6

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ module.exports = {
         "airbnb-base",
     ],
     rules: {
+        "dot-notation": "off",
         "indent": ["error", 4],
         "max-len": ["error", { "code": 120 }],
+        "no-await-in-loop": "off",
+        "no-param-reassign": "off",
+        "no-restricted-syntax": "off",
+        "no-shadow": "off",
+        "no-underscore-dangle": "off",
+        "no-use-before-define": "off",
+        "object-curly-newline": "off",
     }
 }
