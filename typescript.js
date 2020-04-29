47e14d5febd5485de78b49c999c3060e1c60adce
yhchoi <yhchoi@rsupport.com>
2020-04-29 14:45:42 +0900

    build: 0.0.11

--- typescript.js
+++ typescript.js
@@ -12,6 +12,7 @@ module.exports = {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
     }
 }
