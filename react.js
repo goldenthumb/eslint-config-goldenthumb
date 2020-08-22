d43d0435b50fdb5f1481dd3833e720f48358a5fa
yhchoi <cyh8914@gmail.com>
2020-08-22 19:01:35 +0900

    build: 0.0.19

--- react.js
+++ react.js
@@ -1,16 +1,12 @@
 module.exports = {
-    extends: [
-        "airbnb",
-        "airbnb/hooks",
-        "./base.js",
-    ],
+    extends: ['airbnb', 'airbnb/hooks', 'plugin:react/recommended', './base.js'],
     rules: {
-        "react/jsx-closing-tag-location": "off",
-        "react/jsx-filename-extension": "off",
-        "react/jsx-indent": ["error", 4],
-        "react/jsx-indent-props": ["error", 4],
-        "react/jsx-wrap-multilines": "off",
-        "react/prop-types": "warn",
-        "react/require-default-props": "off",
+        'react/jsx-closing-tag-location': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-wrap-multilines': 'off',
+        'react/prop-types': 'warn',
+        'react/require-default-props': 'off',
     },
-}
+};
