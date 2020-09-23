5b22b2c433866e256ddd52c39d30888f9666a191
yhchoi <cyh8914@gmail.com>
2020-09-23 11:44:09 +0900

    update lint

--- react.js
+++ react.js
@@ -1,12 +1,16 @@
 module.exports = {
     extends: ['airbnb', 'airbnb/hooks', 'plugin:react/recommended', './index.js'],
     rules: {
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/interactive-supports-focus': 'off',
         'jsx-a11y/label-has-associated-control': 'off',
         'react-hooks/exhaustive-deps': 'warn',
         'react/jsx-closing-tag-location': 'off',
+        'react/jsx-curly-newline': 'off',
         'react/jsx-filename-extension': 'off',
         'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props': ['error', 4],
+        'react/jsx-props-no-spreading': 'off',
         'react/jsx-wrap-multilines': 'off',
         'react/prop-types': 'off',
         'react/require-default-props': 'off',
